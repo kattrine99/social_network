aecd525c2a5be20de126ab4feac2524a736913d1
kattren99 <kattren990703@gmail.com>
2024-12-05 14:03:29 +0500

    deleted router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import { Header} from "./components/UI/Header/Header";
 import {LoginPage, MainPage, ProfilePage, RegistrationPage } from '@/pages/index'
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 
 export default function App() {
   return (
-    <Router>
       <div className="App">
         <Header />
         <div className="container">
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/auth-login" element={<LoginPage />} />
-            <Route path="/auth-registration" element={< RegistrationPage />} />
-            <Route path="-profile" element={< ProfilePage />} />
+            <Route path="/auth/login" element={<LoginPage />} />
+            <Route path="/auth/registration" element={< RegistrationPage />} />
+            <Route path="/profile" element={< ProfilePage />} />
           </Routes>
         </div>
       </div>
-    </Router>
   );
 }
 

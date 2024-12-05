4ba06ef24c9da9b20ba1139aab4eb2764ca5ef8c
kattren99 <kattren990703@gmail.com>
2024-12-05 14:26:37 +0500

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { Header} from "./components/UI/Header/Header";
 import {LoginPage, MainPage, ProfilePage, RegistrationPage } from '@/pages'
-import {HashRouter, Route, Routes } from "react-router";
+import {BrowserRouter, Route, Routes } from "react-router";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
         <Header />
         <div className="container">
           <Routes>
-            <Route path="/main" element={<MainPage />}>
+            <Route path="/main" element={<MainPage />}/>
             <Route path="login" element={<LoginPage />} />
             <Route path="registration" element={< RegistrationPage />} />
             <Route path="profile" element={< ProfilePage />} />
-            </Route>
+  
           </Routes>
         </div>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
